927401acda5e2204a54b157972899d4a90a0c8f3
Saroj Maharjan <saroj.m2010@gmail.com>
2022-04-01 22:16:17 -0400

    Made Private

--- displayHelper.py
+++ displayHelper.py
@@ -25,18 +25,18 @@ class displayHelper():
     # bus = 0 
     # device = 0 
     logging.basicConfig(level=logging.DEBUG)
-    disp = LCD_2inch.LCD_2inch()
-
+    
     def __init__(self) -> None:
           # display with hardware SPI:
         ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
         #disp = LCD_2inch.LCD_2inch(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
-      
+        self.disp = LCD_2inch.LCD_2inch()
         # Initialize library.
         self.disp.Init()
         # Clear display.
         self.disp.clear()
         self.image, self.draw = self.__newImage()
+        
 
     def __newImage(self):
         # Create blank image for drawing.
@@ -53,7 +53,7 @@ class displayHelper():
     def completeDisplay(self, listOfDisplayProperties):
         for displayProperty in listOfDisplayProperties:
             self.displayText(displayProperty)
-        self.outputDisplay()
+        self.__outputDisplay()
 
     def displayBigText(self,inputStr, color, breakLongWords = True):
         self.__displayText(inputStr, color,90, 7, 3, breakLongWords = breakLongWords)
@@ -62,7 +62,7 @@ class displayHelper():
         self.__displayText(DisplayProperties.text, DisplayProperties.color, DisplayProperties.size, 0, 1, startPosition=DisplayProperties.startPosition)
 
     def __displayText(self, inputStr, color, size, eachLineLen, numOfLines,startPosition = (0,0), breakLongWords = True):
-        startPosition = self.positionPercentToPixel(startPosition)
+        startPosition = self.__positionPercentToPixel(startPosition)
         font3 = ImageFont.truetype("./Font/Capsmall.ttf",size)
         wrappedText = textwrap.wrap(inputStr, eachLineLen, break_long_words=breakLongWords ) if numOfLines > 1 else [inputStr]
         eachLineHeight = (240 - startPosition[1])/numOfLines
@@ -72,7 +72,7 @@ class displayHelper():
             listPosition[1] += eachLineHeight
             startPosition = tuple(listPosition)
 
-    def outputDisplay(self):
+    def __outputDisplay(self):
         try:
             self.image=self.image.rotate(180)
             self.disp.ShowImage(self.image)
@@ -85,7 +85,7 @@ class displayHelper():
             logging.info("quit:")
             exit()
 
-    def positionPercentToPixel(self,position):
+    def __positionPercentToPixel(self,position):
         return (position[0]*self.disp.height/100, position[1]*self.disp.width/100)
 
       
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import displayHelper as dh;
 
 displayH = dh.displayHelper()
 dis1 = dh.DisplayObject("saroj", "red", 100)
-dis2 = dh.DisplayObject("test1", "blue", 50, (10, 50))
+dis2 = dh.DisplayObject("test1", "yellow", 50, (10, 50))
 allDisplay = [dis1, dis2]
 displayH.completeDisplay(allDisplay)
